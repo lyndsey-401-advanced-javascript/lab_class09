b96cbe06a40b9495df2dd216e5be6981a5ea99ae
LYNDSEY THOMAS <THOMAS.LYNDSEY@GMAIL.COM>
2019-08-22 16:34:59 -0700

    repo creation

---
+++ api-server/src/models/todo/todo-model.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const Model = require('../mongo.js');
+const schema = require('./todo-schema.js');
+
+class Todo extends Model {
+  constructor() { super(schema); }
+}
+
+module.exports = Todo;
